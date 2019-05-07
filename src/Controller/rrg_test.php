9095963f608428247fe6f396d9dc0c920a11d5da
Rituraj <rituraj.onnet@gmail.com>
2019-05-07 16:51:55 +0530

    module updated

--- src/Controller/rrg_test.php
+++ src/Controller/rrg_test.php
@@ -4,6 +4,8 @@ namespace Drupal\rrg_test\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\node\Entity\Node;
+use Symfony\Component\Serializer\Serializer;
+
 /**
  * Class rrg_test.
  */
@@ -13,26 +15,32 @@ class rrg_test extends ControllerBase {
    * Nodecheck.
    *
    * @return string
-   *   Return Hello string.
    */
   public function nodecheck($key, $id) {
     $site_config = \Drupal::config('system.site');
-    $siteapikey = $site_config->get('siteapikey');
-    if($siteapikey == $key){
-      $serializer = \Drupal::service('serializer');
-      $node = Node::load($id);
-	  if(!empty($node) && $node->type == 'page' ) {
-        $data = $serializer->serialize($node, 'json', ['plugin_id' => 'entity']);
-        return [
-          '#type' => 'markup',
-          '#markup' => $data,
-        ];
-	  } else {
-	    throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
-      }	  
-    }
-    else{
-      throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException(); 
+    $siteapikey = $site_config->get('siteapikey');	 
+	if(!empty($siteapikey)) {
+	  if($siteapikey == $key) {
+		// Node load  
+		$node = Node::load($id);	   
+		if(!empty($node) && $node->getType() == 'page' ) {		 
+		  $serializer = \Drupal::service('serializer');		 
+		  $data = $serializer->serialize($node, 'json', ['plugin_id' => 'entity']);
+		  return [
+			'#type' => 'markup',
+			'#markup' => $data,
+		  ];
+		} else {
+		  throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException();
+		}	  
+	  }
+	  else{
+		throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException(); 
+	  }
+	}
+	else {
+	  throw new \Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException(); 
     }
   }
+  
 }
